70589c712a30d866d82979be15037c69045ca0fc
IchihanaYue <namioejhh@gmail.com>
2023-08-21 16:27:12 +0700

    commit
    beo

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -21,7 +21,7 @@ func _process(delta):
 	if (hp <=0):
 		die()
 
-# Hallo Im emu otori
+# bueyanyan
 
 func _physics_process(delta):
 	var dir = Input.get_vector("ui_left","ui_right","ui_up","ui_down").normalized()
