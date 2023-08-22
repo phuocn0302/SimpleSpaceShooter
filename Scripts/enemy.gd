4546dc607852ba1bd5ae289d607b35e7a31e265e
phuocn0302 <phuocnguyen230302@gmail.com>
2023-08-22 14:01:01 +0700

    revert enemy movement

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -4,10 +4,9 @@ extends Area2D
 var ExplosionPath = preload("res://Scenes/explosion.tscn")
 
 var contact_damage: int = 1
-var is_rotate : bool = true
 
 @export var hp:int = 5
-@export var speed: float = 80
+@export var speed: float = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,13 +17,6 @@ func _process(delta):
 
 	$AnimationPlayer.play("vibrate")
 	global_position.x -= speed * delta
-	#goofy af movement
-	global_position += ($Node2D/Marker2D.global_position - self.global_position).normalized()
-	if is_rotate :
-		$Node2D.rotate(randf_range(-1.5,1.5))
-		is_rotate = false
-		await get_tree().create_timer(0.1).timeout
-		is_rotate = true
 	
 	if (hp <= 0):
 		die()
