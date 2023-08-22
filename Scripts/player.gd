e71a826c5267868c0bfe920f064a8f38be58daf9
phuocn0302 <phuocnguyen230302@gmail.com>
2023-08-22 13:49:40 +0700

    clean up code and tweak spawn animation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 var BulletPath = preload("res://Scenes/bullet.tscn")
 var ExplosionPath = preload("res://Scenes/explosion.tscn")
 
-var iframe_time: float = 14/60
+var iframe_time: float = 0.3
 
 @export var speed: float = 200.0
 @export var fire_rate: float = 0.1 # Per second
@@ -14,34 +14,33 @@ var can_shoot = true
 var can_take_damage = true
 var can_deal_contact_damage = true
 
+@onready var anim_player = $AnimationPlayer
+@onready var normal_shoot_pos = $NormalShootPos/ShootSpot
+
 func _ready():
-	$CPUParticles2D.emitting = true
-	$AnimationPlayer.play("blinking")
-	await get_tree().create_timer(1).timeout
-	$AnimationPlayer.play("RESET")
+	anim_player.play("spawn")
 
 func _process(delta):
 	if (Input.is_action_pressed("ui_accept")):
 		shoot()
-	
 	if (hp <=0):
 		die()
+
 func _physics_process(delta):
 	var dir = Input.get_vector("ui_left","ui_right","ui_up","ui_down").normalized()
-	velocity = dir * speed
+	velocity = dir * speed * delta * 60
 	move_and_slide()
 
 func shoot():
 	if can_shoot:
 		var bullet = BulletPath.instantiate()
 		get_parent().add_child(bullet)
-		bullet.global_position = $NormalShootPos/ShootSpot.global_position
+		bullet.global_position = normal_shoot_pos.global_position
 		can_shoot = false
 		await get_tree().create_timer(fire_rate).timeout
 		can_shoot = true
 
 func iframe():
-	
 	can_take_damage = false
 	can_deal_contact_damage = false
 	await get_tree().create_timer(iframe_time).timeout
@@ -53,7 +52,7 @@ func take_damage(damage):
 		hp -= damage
 		var hit_anim = ["hit_1", "hit_2"]
 		var rand_hit = hit_anim[randi()% hit_anim.size()]
-		$AnimationPlayer.play(rand_hit)
+		anim_player.play(rand_hit)
 		iframe()
 
 func die():
