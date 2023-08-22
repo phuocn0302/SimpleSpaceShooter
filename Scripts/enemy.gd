8fd967654afa1ba7800de3753b53a5bc53bf1028
phuocn0302 <phuocnguyen230302@gmail.com>
2023-08-22 15:50:03 +0700

    remove unnecessary node

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -5,7 +5,7 @@ var ExplosionPath = preload("res://Scenes/explosion.tscn")
 
 var contact_damage: int = 1
 var is_rotate: bool = true
-
+var is_chasing: bool = false
 
 @export var hp:int = 5
 @export var speed: float = 100
@@ -19,25 +19,25 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$AnimationPlayer.play("vibrate")
-	follow_player(delta)
+	silly_movement(delta)
 	if (hp <= 0):
 		die()
 
 func silly_movement(delta):
-	global_position.x -= speed * delta
-	global_position += ($Node2D/Marker2D.global_position - self.global_position).normalized()
-	if is_rotate == true:
-		$Node2D.rotate(randf_range(-1.5,1.5))
-		is_rotate = false
-		await get_tree().create_timer(0.1).timeout
-		is_rotate = true
+	if not is_chasing:
+		global_position.x -= speed * delta
+		global_position += ($Node2D/Marker2D.global_position - self.global_position).normalized()
+		if is_rotate == true:
+			$Node2D.rotate(randf_range(-1.5,1.5))
+			is_rotate = false
+			await get_tree().create_timer(0.1).timeout
+			is_rotate = true
 
 func follow_player(delta):
 	if get_parent().get_parent().has_node("Player"):
-		global_position += (player.global_position - global_position).normalized() * speed * delta
-	else:
-		silly_movement(delta)
-	
+		is_chasing = true
+		global_position += global_position.direction_to(player.global_position) * speed * delta
+
 func die():
 	var explosion = ExplosionPath.instantiate()
 	get_parent().add_child(explosion)
