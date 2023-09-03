d9f0c50c1f0ad9acdaf6ea951ca10c2d21daa2a0
phuocn0302 <phuocnguyen230302@gmail.com>
2023-09-03 20:29:19 +0700

    remove tween and add timer to dash ghost

--- Scripts/Player/DashGhost.gd
+++ Scripts/Player/DashGhost.gd
@@ -1,14 +1,20 @@
 extends AnimatedSprite2D
 
-var ghost_duration: float = 0.5
+var ghost_duration: float = 0.2
 
 func _ready():
-	var tween:Tween = get_tree().create_tween() #Creates a Tween, as far as I know Tween as a node has been removed
+	$Timer.wait_time = ghost_duration
+	$Timer.start()
+#	var tween:Tween = get_tree().create_tween() #Creates a Tween, as far as I know Tween as a node has been removed
+#
+#	#Sets transition and ease for all following tweens
+#	tween.set_trans(Tween.TRANS_QUART) 
+#	tween.set_ease(Tween.EASE_OUT)
+#
+#	#Tween to execute, 
+#	tween.tween_property(self, "modulate:a", 0.0, ghost_duration)
+#
 
-	#Sets transition and ease for all following tweens
-	tween.set_trans(Tween.TRANS_QUART) 
-	tween.set_ease(Tween.EASE_OUT)
 
-	#Tween to execute, 
-	tween.tween_property(self, "modulate:a", 0.0, ghost_duration)
-
+func _on_timer_timeout():
+	queue_free()
