c49b2d7eab90d7fc2c72d09497e75f75174b2c42
phuocn0302 <phuocnguyen230302@gmail.com>
2023-09-03 20:38:02 +0700

    remove zombie code

--- Scripts/Player/DashGhost.gd
+++ Scripts/Player/DashGhost.gd
@@ -5,16 +5,6 @@ var ghost_duration: float = 0.2
 func _ready():
 	$Timer.wait_time = ghost_duration
 	$Timer.start()
-#	var tween:Tween = get_tree().create_tween() #Creates a Tween, as far as I know Tween as a node has been removed
-#
-#	#Sets transition and ease for all following tweens
-#	tween.set_trans(Tween.TRANS_QUART) 
-#	tween.set_ease(Tween.EASE_OUT)
-#
-#	#Tween to execute, 
-#	tween.tween_property(self, "modulate:a", 0.0, ghost_duration)
-#
-
 
 func _on_timer_timeout():
 	queue_free()
